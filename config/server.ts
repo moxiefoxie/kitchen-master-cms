862b4b07d2af2a9c4f3b839f23e4be974d5de268
moxiefoxie <34554109+moxiefoxie@users.noreply.github.com>
2026-09-22 10:56:12 -0400

    Add cron configuration to server settings

--- config/server.ts
+++ config/server.ts
@@ -9,6 +9,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server =>
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  cron: { enabled: true }
 });
 
 export default config;
